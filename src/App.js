5e2e0c09992c97abbf7593e308508a10d049ac14
Eric Hendrix <agentmrbig@gmail.com>
2019-06-05 21:33:21 -0400

    Declared all routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+ 
+
 
 const App = () => (
   <div>
+
     <Navigation />
-  </div>
+    
+    <Route exact path="/" render={
+      () => {
+        return ( <h1>Welcome Home</h1>);
+      }
+    }/>
+
+    <Route path="/about" render={
+      () => {
+        return ( <h1>About</h1>);
+      }
+    }/>
+
+    <Route path="/contact" render={
+      () => {
+        return ( <h1>Contact</h1>);
+      }
+    }/>
+    </div>
 );
 
 export default App;
